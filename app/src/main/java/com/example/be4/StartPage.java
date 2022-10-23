d126d109f8ed9a764c14d48f016cabab781d26af
Damish-N <damishnisal100@gmail.com>
2022-10-23 14:31:38 +0530

    Clickable the linear layout

--- app/src/main/java/com/example/be4/StartPage.java
+++ app/src/main/java/com/example/be4/StartPage.java
@@ -3,14 +3,38 @@ package com.example.be4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class StartPage extends AppCompatActivity {
+
+     private  LinearLayout supervisorBtn, ownerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
 
+
         setContentView(R.layout.activity_start_page);
+        supervisorBtn = (LinearLayout) findViewById(R.id.supervierBtn);
+        supervisorBtn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(StartPage.this, "Clicked owner", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+        ownerBtn = (LinearLayout) findViewById(R.id.ownerBtn);
+        ownerBtn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(StartPage.this, "Clicked Supervisor", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
 }
