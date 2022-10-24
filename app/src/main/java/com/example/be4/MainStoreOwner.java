b35f4230283799cbb12b3a5a9cfe533b379980ef
Damish-N <damishnisal100@gmail.com>
2022-10-25 01:52:36 +0530

    Retrieve data from backend

--- app/src/main/java/com/example/be4/MainStoreOwner.java
+++ app/src/main/java/com/example/be4/MainStoreOwner.java
@@ -1,5 +1,6 @@
 package com.example.be4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -8,10 +9,21 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
 
 import com.example.be4.models.ProgramAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -19,11 +31,17 @@ import java.util.Objects;
 public class MainStoreOwner extends AppCompatActivity {
 
     private ListView languageLV;
-    private Button addBtn,updateItems;
+    private Button addBtn, updateItems;
     private EditText itemEdt;
     private ArrayList<String> lngList;
+    LinearLayout editArea;
+    ScrollView viewArea;
+    ProgressBar progressBar;
 
-    ArrayList<String> itemNames = new ArrayList<>() ;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference cities = db.collection("cities");
+
+    ArrayList<String> itemNames = new ArrayList<>();
     ArrayList<String> itemCounts = new ArrayList<>();
 
     @SuppressLint("MissingInflatedId")
@@ -32,17 +50,45 @@ public class MainStoreOwner extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_store_owner);
         Objects.requireNonNull(getSupportActionBar()).hide();
+//        progressBar.findViewById(R.id.progressBarOwner);
+//        progressBar.setVisibility(View.VISIBLE);
+        editArea = findViewById(R.id.editArea);
+        viewArea = findViewById(R.id.viewArea);
+        editArea.setVisibility(View.GONE);
+        viewArea.setVisibility(View.GONE);
+
+        db.collection("items").get()
+                .addOnCompleteListener(
+                        new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        System.out.println(document.getData().get("count"));
+                                        itemNames.add(document.getData().get("itemName").toString());
+                                        itemCounts.add(document.getData().get("count").toString());
+                                    }
+//                                    progressBar.setVisibility(View.VISIBLE);
+                                    editArea.setVisibility(View.VISIBLE);
+                                    viewArea.setVisibility(View.VISIBLE);
+                                } else {
+                                    Toast.makeText(MainStoreOwner.this, "empty", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                );
+
 
         languageLV = findViewById(R.id.mainItem);
         addBtn = findViewById(R.id.addItem);
-        updateItems=findViewById(R.id.updateItems);
+        updateItems = findViewById(R.id.updateItems);
         itemEdt = findViewById(R.id.idEdtItemName);
 
-        itemNames.add("item-1");
-        itemNames.add("item-2");
-
-        itemCounts.add("1");
-        itemCounts.add("5");
+//        itemNames.add("item-1");
+//        itemNames.add("item-2");
+//
+//        itemCounts.add("1");
+//        itemCounts.add("5");
 
 
         ProgramAdapter programAdapter = new ProgramAdapter(this, itemNames, itemCounts);
