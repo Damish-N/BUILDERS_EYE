e28b92f3ec221db8c2e40ab3fd806137aeb9d890
Damish-N <damishnisal100@gmail.com>
2022-10-26 03:17:32 +0530

    Supervisor Part

--- app/src/main/java/com/example/be4/StartPage.java
+++ app/src/main/java/com/example/be4/StartPage.java
@@ -26,7 +26,10 @@ public class StartPage extends AppCompatActivity {
         );
         LinearLayout ownerBtn = (LinearLayout) findViewById(R.id.ownerBtn);
         ownerBtn.setOnClickListener(
-                view -> Toast.makeText(StartPage.this, "Clicked Supervisor", Toast.LENGTH_SHORT).show()
+                view -> {
+//                    Intent intent = new Intent(StartPage.this,)
+                    Toast.makeText(StartPage.this, "Clicked Supervisor", Toast.LENGTH_SHORT).show();
+                }
         );
     }
 }
---
+++ app/src/main/java/com/example/be4/SuperviserHomePage.java
@@ -0,0 +1,14 @@
+package com.example.be4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuperviserHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_superviser_home_page);
+    }
+}
