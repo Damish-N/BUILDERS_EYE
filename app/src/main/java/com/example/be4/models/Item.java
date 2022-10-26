78f576f66c99c53bed877aba1264dac79b26dd79
Damish-N <damishnisal100@gmail.com>
2022-10-26 09:28:36 +0530

    Ongoing sites showings with the supervisor

--- app/src/main/java/com/example/be4/models/Item.java
+++ app/src/main/java/com/example/be4/models/Item.java
@@ -9,5 +9,19 @@ public class Item {
         this.count = count;
     }
 
+    public String getItemName() {
+        return itemName;
+    }
 
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
